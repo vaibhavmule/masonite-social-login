97d57a5553f4060c42b77b1b80e1a6f93edb8663
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-09-18 15:48:20 +0530

    optimize the code

--- app/drivers/SocialAuthTwitterDriver.py
+++ app/drivers/SocialAuthTwitterDriver.py
@@ -5,13 +5,9 @@ from masonite.drivers.BaseDriver import BaseDriver
 
 class SocialAuthTwitterDriver(BaseDriver):
 
-    request_token_url = 'https://api.twitter.com/oauth/request_token'
-    authorization_base_url = 'https://api.twitter.com/oauth/authenticate'
-    access_token_url = 'https://api.twitter.com/oauth/access_token'
-    request_token = ''
-
-    def __init__(self, Request, SocialAuthConfig):
+    def __init__(self, Request, SocialAuthConfig, Application):
         self.config = SocialAuthConfig
+        self.appconfig = Application
         self.request = Request
         self.twitter = OAuth1Session(
             self.config.DRIVERS['twitter']['consumer_key'],
@@ -19,14 +15,18 @@ class SocialAuthTwitterDriver(BaseDriver):
         )
 
     def auth(self):
-        request_token = self.twitter.fetch_request_token(self.request_token_url)
-        self.request_token = request_token  
-        return self.request.redirect(self.twitter.authorization_url(self.authorization_base_url))
+        request_token = self.twitter.fetch_request_token('https://api.twitter.com/oauth/request_token')
+        auth_url = self.twitter.authorization_url('https://api.twitter.com/oauth/authenticate')
+        self.request.session.set('request_token', request_token)
+        return self.request.redirect(auth_url)
 
     def callback(self):
         verifier = self.request.input('oauth_verifier')
+        token = self.request.session.get('request_token')
+        self.request.session.delete('request_token')
 
-        self.twitter.resource_owner_key = self.request_token['oauth_token']
-        self.twitter.resource_owner_secret = self.request_token['oauth_token_secret']
-        
-        return self.twitter.fetch_access_token(self.access_token_url, verifier=verifier)
+        self.twitter.resource_owner_key = token['oauth_token']
+        self.twitter.resource_owner_secret = token['oauth_token_secret']
+
+        self.twitter.fetch_access_token('https://api.twitter.com/oauth/access_token', verifier=verifier)
+        return self.request.redirect('/')
