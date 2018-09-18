74c9e71ff7bd2e905cc5f2b6332bb567d9c02be3
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-09-18 16:15:57 +0530

    change self.twitter to self.oauth

--- app/drivers/SocialAuthTwitterDriver.py
+++ app/drivers/SocialAuthTwitterDriver.py
@@ -5,18 +5,17 @@ from masonite.drivers.BaseDriver import BaseDriver
 
 class SocialAuthTwitterDriver(BaseDriver):
 
-    def __init__(self, Request, SocialAuthConfig, Application):
+    def __init__(self, Request, SocialAuthConfig):
         self.config = SocialAuthConfig
-        self.appconfig = Application
         self.request = Request
-        self.twitter = OAuth1Session(
+        self.oauth = OAuth1Session(
             self.config.DRIVERS['twitter']['consumer_key'],
             client_secret=self.config.DRIVERS['twitter']['consumer_secret'],
         )
 
     def auth(self):
-        request_token = self.twitter.fetch_request_token('https://api.twitter.com/oauth/request_token')
-        auth_url = self.twitter.authorization_url('https://api.twitter.com/oauth/authenticate')
+        request_token = self.oauth.fetch_request_token('https://api.twitter.com/oauth/request_token')
+        auth_url = self.oauth.authorization_url('https://api.twitter.com/oauth/authenticate')
         self.request.session.set('request_token', request_token)
         return self.request.redirect(auth_url)
 
@@ -25,8 +24,8 @@ class SocialAuthTwitterDriver(BaseDriver):
         token = self.request.session.get('request_token')
         self.request.session.delete('request_token')
 
-        self.twitter.resource_owner_key = token['oauth_token']
-        self.twitter.resource_owner_secret = token['oauth_token_secret']
+        self.oauth.resource_owner_key = token['oauth_token']
+        self.oauth.resource_owner_secret = token['oauth_token_secret']
 
-        self.twitter.fetch_access_token('https://api.twitter.com/oauth/access_token', verifier=verifier)
+        self.oauth.fetch_access_token('https://api.twitter.com/oauth/access_token', verifier=verifier)
         return self.request.redirect('/')
