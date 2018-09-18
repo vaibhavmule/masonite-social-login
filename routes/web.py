2cc85286b9fdc11f31f295585394d968fdccea7b
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-09-18 10:53:36 +0530

    wip routes

--- routes/web.py
+++ routes/web.py
@@ -1,5 +1,7 @@
 ''' Web Routes '''
 from masonite.routes import Get, Post
+from masonite.helpers.routes import get
+from social_core.actions import do_auth, do_complete, do_disconnect
 
 ROUTES = [
     Get().route('/', 'WelcomeController@show'),
@@ -8,4 +10,13 @@ ROUTES = [
     Get().route('/auth', 'OauthController@send_to_twitter'),
     Get().route('/complete/twitter', 'OauthController@complete'),
     Get().route('/logout', 'OauthController@logout'),
+
+    # Generic Routes
+    RouteGroup([
+        get('/url1', ...).name('create'),
+        get('/url2', ...).name('update'),
+        get('/url3', ...).name('delete'),
+    ], 
+    prefix='/auth',
+    ),
 ]
